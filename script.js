485e198ab540fc8aabb618054b64a079adf67d5c
Rowanbiets <Rowanbiets@users.noreply.github.com>
2024-11-26 08:33:22 +0000

    deploy: 51fbf3d7ed6fb8bdf1ded7789d2c390448d3c440

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault(); // Voorkom dat de pagina herlaadt
-    const name = document.getElementById("name").value;
-    const message = `Thank you, ${name}, for reaching out!`;
-    document.getElementById("formMessage").innerText = message;
-    this.reset(); // Reset het formulier
-  });
-  
